b2a7bda44cf2ad9b214337f8b440ed99ade16e34
Pushtibhatt09 <bhattpd09@gmail.com>
2025-04-01 12:34:01 +0530

    Removed ignore files

--- base/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .users import User
-from .store import Store, Product, Category, ProductImage, Review
-from .orders import Order
-from .cart import Cart
-from .recommendations import Recommendation
